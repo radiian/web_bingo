4827cd2462cffa3938d59db3313d310b1bb2a20d
Radiian <thompzac@oregonstate.edu>
2020-09-07 00:23:51 -0700

    comment

--- bingo.php
+++ bingo.php
@@ -4,7 +4,7 @@ require_once('db.php');
 $con = mysqli_connect($db_server, $db_user, $db_password, $db_name);
 
 if(!$con){
-	die("Something went wrong");
+        die("Something went wrong");
 }
 
 
@@ -13,41 +13,42 @@ $squares = mysqli_query($con, $query);
 
 $text = array();
 if(mysqli_num_rows($squares) > 0){
-	while($row = mysqli_fetch_assoc($squares)){
-		array_push(text, $row['text']);
-	}
-	
-	echo $text;
-	
-	$table = "<table>";
-	
-	$idx = 0;
-	for($i = 0; $i < 5; ++$i){
-		$table .= "<tr>";
-		if($i == 2){
-			//be sure to add the free space
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>Free Space</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-		
-		}
-		else{
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-			$table .= "<th>". $text[$idx++] . "</th>";
-		}
-		$table .= "</tr>";
-	
-	}
-	$table .= "</table>";
-	echo $table;
+        while($row = mysqli_fetch_assoc($squares)){
+                array_push($text, $row['text']);
+        }
+
+
+        $table = "<table>";
+
+        $idx = 0;
+        for($i = 0; $i < 5; ++$i){
+                $table .= "<tr height='100px' width='100px'>";
+                if($i == 2){
+                        //be sure to add the free space
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>Free Space</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+
+                }
+                else{
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                        $table .= "<th>". $text[$idx++] . "</th>";
+                }
+                $table .= "</tr>";
+
+        }
+        $table .= "</table>";
+        echo "<html><head><style>table, th, td {  word-wrap:break-word;border: 1px solid black;  border-collapse: collapse;} table {table-layout: fixed;}</style></head>";
+        echo $table;
+        echo "</html>";
 }
 else{
-	echo "No data :(";
+        echo "No data :(";
 }
 
 ?>
