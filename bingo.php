b0204df71f7f38d74e6d1d15ec49b5aee23f1ce7
Radiian <thompzac@oregonstate.edu>
2020-09-07 00:08:51 -0700

    Shit should be in table form now

--- bingo.php
+++ bingo.php
@@ -3,7 +3,7 @@ require_once('db.php');
 
 $con = mysqli_connect($db_server, $db_user, $db_password, $db_name);
 
-if($!con){
+if(!$con){
 	die("Something went wrong");
 }
 
@@ -18,6 +18,33 @@ if(mysqli_num_rows($squares) > 0){
 	}
 	
 	echo $text;
+	
+	$table = "<table>";
+	
+$idx = 0;
+for($i = 0; $i < 5; ++$i){
+	$table += "<tr>";
+	if($i == 2){
+		//be sure to add the free space
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>Free Space</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		
+	}
+	else{
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+		$table += "<th>". $text[$idx++] . "</th>";
+	}
+	$table += "</tr>";
+	
+}
+
+echo $table;
 }
 else{
 	echo "No data :(";
