0917f98f11804b14d2b93069411d2e4cb2afa12c
Radiian <thompzac@oregonstate.edu>
2020-09-06 23:34:17 -0700

    SQL is setup, lets test

--- bingo.php
+++ bingo.php
@@ -1,4 +1,22 @@
 <?php
 require_once 'db.php'
 
+$con = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+if($!con){
+	die("Something went wrong");
+}
+
+$query = "SELECT * FROM squares ORDER BY RAND() LIMIT 24";
+$squares = mysqli_query($con, $query);
+
+if(mysqli_num_rows($squares) > 0){
+	while($row = mysqli_fetch_assoc($squares)){
+		echo $row['text']. "<br/>";
+	}
+}
+else{
+	echo "No data :(";
+}
+
 ?>
